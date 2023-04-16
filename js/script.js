348211c01125bb8bcc31779b9f017ba4e4df029e
Behtmoth-Jr <79050532+Behtmoth-Jr@users.noreply.github.com>
2023-04-16 18:39:33 +0300

    51

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
 let image = document.getElementById("image");
 let file = document.getElementById("file");
 
 file.addEventListener('change', function(){
-  image.src = URL.createObjectURL(file.files[0]);
-  image.style.display = "block";
+    image.src = URL.createObjectURL(file.files[0]);
+    image.style.display = "block";
 });
